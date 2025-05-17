d681dbab4e64e4a415fdeb9d0123f18b4a3992ca
Juan David Ramón <jdramons30@gmail.com>
2025-05-17 16:31:22 -0500

    commit -m “Edit  Categories”

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,42 @@
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupermarketWEB.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; } = default!;
+        //
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category != null)
+            {
+                Category = category;
+                _context.Categories.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+    }
+}
+    
