43175b22a1f10add9700934479a52393a65cb058
Juan David Ramón <jdramons30@gmail.com>
2025-05-17 22:56:31 -0500

    Commit -m" Se Visualiza User"

---
+++ Migrations/20250518030835_Providers.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SupermarketWEB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Providers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProviderId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Providers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Providers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProviderId",
+                table: "Products",
+                column: "ProviderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Providers_ProviderId",
+                table: "Products",
+                column: "ProviderId",
+                principalTable: "Providers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Providers_ProviderId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Providers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProviderId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ProviderId",
+                table: "Products");
+        }
+    }
+}
---
+++ Pages/Providers/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public DeleteModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Provider Provider { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers.FindAsync(id);
+
+            if (provider != null)
+            {
+                Provider = provider;
+                _context.Providers.Remove(Provider);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
